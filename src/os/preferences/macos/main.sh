0200f1d09984a93d9cb0014a3965ad0f38c611cf
Voo Sierra <anybucket@gmail.com>
2017-05-20 22:42:56 +0800

    update on 5-19-17:5 airra dotfiles

--- src/os/preferences/macos/main.sh
+++ src/os/preferences/macos/main.sh
@@ -11,19 +11,19 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-./app_store.sh
-./chrome.sh
-./dashboard.sh
-./dock.sh
-./finder.sh
-./firefox.sh
-./keyboard.sh
-./language_and_region.sh
-./maps.sh
-./photos.sh
-./safari.sh
-./terminal.sh
-./textedit.sh
-./trackpad.sh
+#./app_store.sh
+#./chrome.sh
+#./dashboard.sh
+#./dock.sh
+#./finder.sh
+#./firefox.sh
+#./keyboard.sh
+#./language_and_region.sh
+#./maps.sh
+#./photos.sh
+#./safari.sh
+#./terminal.sh
+#./textedit.sh
+#./trackpad.sh
 ./transmission.sh
-./ui_and_ux.sh
+#./ui_and_ux.sh
